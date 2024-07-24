64f835194b282e9c80335f2176bf0a14a043c5cc
Vwalln99 <98455097+Vwalln99@users.noreply.github.com>
2024-07-24 09:16:14 +0200

    Update home.js

--- src/home.js
+++ src/home.js
@@ -6,7 +6,7 @@ import './styles.css';
 function createHomeContent(){
     const home_section = createTag(null, "section", "home_section");
     const homeh1 = createTag(home_section, "h1", null, "homeh1", "Herzlich Willkommen im Restaurant Bauklotz!");
-    const homeh2 = createTag(home_section, "h3", null, "homeh2", "Wie funktioniert das ganze?");
+    const homeh2 = createTag(home_section, "h3", null, "homeh2", "Wie funktioniert das Ganze?");
     const homeText = createTag(home_section, "p", null, "home_text", 
     "Wähle dein Gericht"+ "<br>"+
     "Wähle deine Lieblingszutaten"+ "<br>"+
